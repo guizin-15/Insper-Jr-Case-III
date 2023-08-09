e37d5ae75afa5121c0225aa392abab3ec4afc725
joaopgs4 <joaopgs4@al.insper.edu.br>
2023-08-09 13:09:45 -0300

    Feat: Profile editor functionality added

---
+++ user_edit/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+from user_create.models import Profile
+from django import forms
+
+#Just as the user creation form, well custom the django origin form to support the desired fields
+class UserEditForm(UserChangeForm):
+    first_name = forms.CharField(max_length=150, required=True)
+    last_name = forms.CharField(max_length=150, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+
+    #Profile image awaiting implementation
+    #profile_img = forms.ImageField(required=False)
+
+    phone_number = forms.CharField(max_length=19, required=True)
+    gender = forms.ChoiceField(choices=Profile.Genders, required=True)
+
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'gender',
+        ]
