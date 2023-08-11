0244bd27e63c61c842f369979f4fa67d98895449
Andre Pereira <deco9871@gmail.com>
2023-08-11 00:25:21 -0300

    feat: appointments page

--- appointments/views.py
+++ appointments/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def appointments(request):
-    return render(request, 'appointments.html')
+    user = request.user
+    profile = user.profile
+    img = profile.profile_img
+    profile_img = "/" + str(img)
+
+    return render(request, 'appointments.html', {"profile_img": profile_img})
 
 def schedule(request): 
     return render(request, 'schedule.html')
