2ede9841e16f10a6cb51bf2bc01ba39efad5d25d
Andre Pereira <deco9871@gmail.com>
2023-08-11 00:42:45 -0300

    dev

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.appointments, name='appointments'),
+    path('funcionario/', views.empl_appointments, name='empl-appointments'),
     path('schedule/', views.schedule, name='schedule')
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def empl_appointments(request):
+    user = request.user
+    profile = user.profile
+    img = profile.profile_img
+    profile_img = "/" + str(img)
+
+    return render(request, 'empl_appointments.html', {"profile_img": profile_img})
+
+# Create your views here.
 def appointments(request):
     user = request.user
     profile = user.profile
